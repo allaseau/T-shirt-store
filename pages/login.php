92c332dfe2d6b3d46d696cea02e662dc7b864262
Dylan Allart <chenoa.299@gmail.com>
2022-05-27 19:52:39 +0200

    logo clique

--- pages/login.php
+++ pages/login.php
@@ -6,15 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../src/css/style.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>T-shirt Store</title>
 </head>
 
 <body>
     <header>
-        <img class="logo" src="../src/img/logo1.png" alt="Logo Store" />
+        <a href="index.php">
+            <img class="logo" src="../src/img/logo1.png" alt="Logo Store" />
+        </a>
         <nav>
             <div id="wrap">
                 <ul class="navbar">
