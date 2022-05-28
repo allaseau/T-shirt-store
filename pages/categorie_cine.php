050287e66b82df3e9a4858a8325e890b52f97993
Dylan Allart <chenoa.299@gmail.com>
2022-05-28 19:36:46 +0200

    switch link

--- pages/categorie_cine.php
+++ pages/categorie_cine.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../src/css/style.min.css">
+    <link rel="stylesheet" href="../static/css/style.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>T-shirt Store</title>
 </head>
@@ -13,7 +13,7 @@
 <body>
     <header>
         <a href="index.php">
-            <img class="logo" src="../src/img/logo1.png" alt="Logo Store" />
+            <img class="logo" src="../static/img/logo1.png" alt="Logo Store" />
         </a>
         <nav>
             <div id="wrap">
@@ -72,7 +72,7 @@
             <section class="footer__nav">
                 <div>
                     <a href="index.php">
-                        <img class="logo__foot" src="../src/img/logo1.png" alt="Logo Store" />
+                        <img class="logo__foot" src="../static/img/logo1.png" alt="Logo Store" />
                     </a>
                     <nav>
                         <ul>
