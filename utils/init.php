8c94652d2b3f8b9f908de35158d7c0320b356ca5
Dylan Allart <chenoa.299@gmail.com>
2022-05-30 23:01:56 +0200

    correct path

--- utils/init.php
+++ utils/init.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-$folder_root = '/T-shirt-store-main/';
+$folder_root = '/T-shirt-store/';
 $root_path = $_SERVER['DOCUMENT_ROOT'] . $folder_root;
 
 require_once($root_path . '/utils/utils.php');
