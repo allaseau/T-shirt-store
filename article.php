5b88d3c3c0092b1c9fcc07cf0ebafd6994b4ebe0
Dylan Allart <chenoa.299@gmail.com>
2022-05-30 23:24:37 +0200

    debug

--- article.php
+++ article.php
@@ -8,13 +8,12 @@ $article = get_article_by_id($_GET['id']);
 <?php require_once('./components/head.php'); ?>
 
 <body>
-<?php require_once('./components/header.php'); ?>
+    <?php require_once('./components/header.php'); ?>
 
-<main>
-    <div class="col-md-4 mb-4">
-        <a href="./article.php?id=<?= $article['id'] ?>">
+    <main>
+        <div class="col-md-4 mb-4">
             <article class="card">
-                <img src="<?= $article['image']; ?>" alt="Image de <?= $article['nom']; ?>" class="card-img-top"/>
+                <img src="<?= $article['image']; ?>" alt="Image de <?= $article['nom']; ?>" class="card-img-top" />
                 <div class="card-body">
                     <h5 class="card-title"><?= $article['nom'] ?></h5>
                     <p class="card-text"><?= $article['createur'] ?></p>
@@ -22,11 +21,10 @@ $article = get_article_by_id($_GET['id']);
                     <p class="card-text"><?= $article['description'] ?></p>
                 </div>
             </article>
-        </a>
-    </div>
-</main>
+        </div>
+    </main>
 
-<?php require_once('./components/footer.php'); ?>
+    <?php require_once('./components/footer.php'); ?>
 </body>
 
 </html>
